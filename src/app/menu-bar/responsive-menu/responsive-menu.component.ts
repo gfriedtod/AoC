fd6e6bfc4abd98a4815eaf78dc0e6961296be567
gfried-code <wilfried.wayou@2026.ucac-icam.com>
2023-05-26 14:47:11 +0100

    web-project

---
+++ src/app/menu-bar/responsive-menu/responsive-menu.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {BouttonService} from "../../boutton/bouttonService/BouttonService";
+import {animate, animation, keyframes, style, transition, trigger} from "@angular/animations";
+
+// @ts-ignore
+@Component({
+  selector: 'app-responsive-menu',
+  templateUrl: './responsive-menu.component.html',
+  styleUrls: ['./responsive-menu.component.scss'],
+  animations: [ trigger('statechange', [
+    transition(':enter', [
+      animate('0.5s', keyframes([
+        style( {transform: 'translateX(-100%)' ,offset: 0}),
+        style({transform: 'translateX(0)', offset: 1})
+      ]))
+    ])
+
+  ]) ]
+})
+export class ResponsiveMenuComponent implements  OnInit{
+   stateviewLogIn!: BouttonService;
+   stateviewSign!: BouttonService;
+  ngOnInit(): void {
+
+    this.stateviewLogIn = new BouttonService('LogIn' )
+    this.stateviewSign= new BouttonService('Sign up' , true)
+
+  }
+
+}
