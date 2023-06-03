31587c5c2b5bf81a0900937f8b183bb4cb916909
gfried-code <wilfried.wayou@2026.ucac-icam.com>
2023-06-03 21:30:53 +0100

    web-project

--- src/app/dashbord/dashbord.component.ts
+++ src/app/dashbord/dashbord.component.ts
@@ -11,49 +11,78 @@ import {BouttonService} from "../component/boutton/bouttonService/BouttonService
 })
 export class DashbordComponent  implements OnInit{
  // fileName!: string;
+ //
+ //  myForm!: FormGroup;
+ //  key_list!: FormArray;
+ //  addboutton: BouttonService = new BouttonService('+add key');
+ //
+ //  constructor(private fb: FormBuilder) { }
+ //
+ //  ngOnInit() {
+ //
+ //    this.myForm = this.fb.group({
+ //      title: ['', Validators.required],
+ //      objectif: [''],
+ //      Identitytitle: ['', Validators.required],
+ //      Identitydescription: [''],
+ //      mission_title: [''],
+ //      mission_description: [''],
+ //      key_list: this.fb.array([]),
+ //    });
+ //
+ //
+ //
+ //  }
+ //
+ //  addKey() {
+ //
+ //    console.log(this.key_list)
+ //
+ //    if (this.key_list === undefined) {
+ //      this.key_list = this.myForm.get('key_list') as FormArray;
+ //    }else {
+ //      this.key_list.push(this.fb.group({
+ //        key: ['', Validators.required],
+ //        key_description: ['']
+ //      }));
+ //    }
+ //
+ //  }
+ //  onSubmit() {
+ //    if (this.myForm.invalid) {
+ //      return;
+ //    }
+ //
+ //    const formData = this.myForm.value;
+ //    console.log(formData);
+ //  }
 
   myForm!: FormGroup;
   key_list!: FormArray;
   addboutton: BouttonService = new BouttonService('+add key');
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
-
+  ngOnInit(): void {
     this.myForm = this.fb.group({
-      title: ['', Validators.required],
-      objectif: [''],
-      Identitytitle: ['', Validators.required],
-      Identitydescription: [''],
-      mission_title: [''],
-      mission_description: [''],
+      title: '',
+      Identitytitle: '',
+      Identitydescription: '',
       key_list: this.fb.array([]),
     });
-
-
-
   }
 
-  addKey() {
+  addKey(): void {
+    this.key_list = this.myForm.get('key_list') as FormArray;
+    this.key_list.push(this.fb.group({
+      key: '',
+      key_description: '',
+    }));
 
-    console.log(this.key_list)
-
-    if (this.key_list === undefined) {
-      this.key_list = this.myForm.get('key_list') as FormArray;
-    }else {
-      this.key_list.push(this.fb.group({
-        key: ['', Validators.required],
-        key_description: ['']
-      }));
-    }
-
+    console.log('myForm after:', this.myForm.value);
   }
-  onSubmit() {
-    if (this.myForm.invalid) {
-      return;
-    }
 
-    const formData = this.myForm.value;
-    console.log(formData);
+  onSubmit(): void {
+    console.log(this.myForm.value);
   }
 }
